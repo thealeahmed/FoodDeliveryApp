321dac89045c0e9e23c35dd7c7a9a09bb013f38e
thealeahmad <aliahmad111486@gmail.com>
2025-08-12 17:06:19 +0500

    Understood what routing is how can i implement it i have implemented in my code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,55 +1,38 @@
-import RestaurantCard  from "./RestaurantCard";
-import resList from "../utils/mockData";  
-import { useState,useEffect } from "react"; 
-import { ClipLoader } from "react-spinners";  
-import { Shimmer } from "./Shimmer";    
-const Body =()=>{
- const [listOfRestaurant,setlistOfRestaurant]=useState([]);
- const[searchText,setSearchText]=useState("");
- const[filteredRestaurant,setfilteredRestaurant]=useState();
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import { ClipLoader } from "react-spinners";
+import { Shimmer } from "./Shimmer";
+const Body = () => {
+  const [listOfRestaurant, setlistOfRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurant, setfilteredRestaurant] = useState([]);
 
  useEffect(()=>{ 
-        fetchData();
+    fetchData();
     },[])
-        //  
-        const fetchData= async()=>{
-            const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-                );
-                const json=await data.json(); 
-                console.log(json)     
-                const gridWidgetCard = json.data.cards.find(
-                 (c) => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.gandalf.widgets.v2.GridWidget"
-                  );
-                  //Optional Chaining
-                const restaurants = gridWidgetCard?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
-
-                console.log('restaurants', restaurants)
-                setlistOfRestaurant(restaurants);
-                setfilteredRestaurant(restaurants)
-        }
-        //conditional rendering
-     if (listOfRestaurant.length === 0) {
-        return <Shimmer/> 
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "100vh"
-//       }}
-//     >
-//       <ClipLoader color="#36d7b7" size={50} />
-//     </div>
-//   );
-}
-//ternary operator
+  //
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const res = await data.json();
+    console.log(res);
+    const gridWidgetCard = res.data.cards.find(
+      (c) =>
+        c.card?.card?.gridElements?.infoWithStyle?.["@type"] ===
+        "type.googleapis.com/swiggy.seo.widgets.v1.FoodRestaurantGridListingInfo"
+    );
+    //Optional Chaining
+    const restaurants =
+      gridWidgetCard?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setlistOfRestaurant(restaurants);
+    setfilteredRestaurant(restaurants);
+  };
+  //ternary operator
     return (listOfRestaurant.length === 0)?  <Shimmer/> : (
-        <div>
-            {/* <div className='search-bar'>Search
+    <div>
+      {/* <div className='search-bar'>Search
             </div> */}
-            <div className="filter">
-               <div className="search">
+      <div className="filter">
+        <div className="search">
                 <input type="text"  className="search-bar" value={searchText} onChange=
                 {(e)=>{
                    setSearchText(e.target.value) 
@@ -64,35 +47,33 @@ const Body =()=>{
                         }  
                     }>Search
                    
-                </button>
-                 
-               </div>
-                <button className="filter-btn" 
-                onClick={()=> {
-                    const topRated = listOfRestaurant.filter(
-                     (res) => res.info.avgRating > 4.5
-                    );
-                setlistOfRestaurant (topRated);
-                }}
-                >
-                    Top Rated Restaurant</button>
-            </div>
-            <div className='restaurant-container'>
-                {/* //restaurantCard */}
-
-                {Array.isArray(filteredRestaurant) && filteredRestaurant.map(restaurants => (
-                 <RestaurantCard 
-            key={restaurants.info.id}
-            resData={restaurants}
-        />
-    ))}
-            </div>
-
+          </button>
         </div>
-    )
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const topRated = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setfilteredRestaurant(topRated);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="restaurant-container">
+        {/* //restaurantCard */}
+
+        {Array.isArray(filteredRestaurant) &&
+          filteredRestaurant.map((restaurants) => (
+            <RestaurantCard key={restaurants.info.id} resData={restaurants} />
+          ))}
+      </div>
+    </div>
+  )
 }
 export default Body;
 
 //when the body component is render as soon as the render is finished it will call
 //the callback function in useEffect
-//useEffect takes two arguments first is callback fn and the second is dependency array 
+//useEffect takes two arguments first is callback fn and the second is dependency array
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -6,7 +6,7 @@ export const Shimmer=()=>{
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
-             <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
         </div>
     )
 }
